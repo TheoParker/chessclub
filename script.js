1f3a4ec33dbbdf1fc4be60c7f83c2b363c71576b
Mitch (local) <mitch.parker@outlook.com>
2021-05-02 10:35:41 -0700

    add timeout to update page on status change

--- script.js
+++ script.js
@@ -42,12 +42,6 @@ var getTeamMembers = function (team) {
         status: xhttp.status,
         statusText: xhttp.statusText
       });
-      // resolve(
-      //   [
-      //     { id: 'tparker24' },
-      //     { id: 'mitch-parker' }
-      //   ]
-      // );
     };
     xhttp.send();
   });
@@ -55,7 +49,6 @@ var getTeamMembers = function (team) {
 
 var getMemberRating = (member, idx) => {
   return new Promise((resolve, reject) => {
-    // delay idx * 200ms
     sleep(250);
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", "https://lichess.org/api/user/" + member + "/rating-history", true);
@@ -81,97 +74,101 @@ var getMemberRating = (member, idx) => {
 function drawRatingHistory() {
 
   document.getElementById('loading_status').innerHTML = "loading team members...";
-  getTeamMembers('shp-chess-club').then(mbrs => {
-    // console.log(mbrs.map(m => m.id));
-    let members = mbrs.map(m => m.id);
-    const maxMembers = 20;
-    const startMember = -59;
-    members = (startMember > (- maxMembers - 1))
-      ? members.slice(-maxMembers)
-      : members.slice(startMember, startMember + maxMembers);
-    console.log(members);
-    var data = new google.visualization.DataTable();
-    data.addColumn('number', 'changeDate');
+  getTeamMembers('shp-chess-club')
+    .then(mbrs => {
+      console.log(mbrs.map(m => m.id));
+      console.log("loading team members' history...");
+      document.getElementById('loading_status').innerHTML = "loading team members' history...";
+      setTimeout(() => {
 
-    let ratingPromises = [];
-    for (let i = 0; i < members.length; i++) {
-      ratingPromises.push(getMemberRating(members[i], i));
-    }
+        let members = mbrs.map(m => m.id);
+        const maxMembers = 15;
+        const startMember = 0 //-59;
+        members = (startMember > (- maxMembers - 1))
+          ? members.slice(-maxMembers)
+          : members.slice(startMember, startMember + maxMembers);
+        console.log(members);
+        var data = new google.visualization.DataTable();
+        data.addColumn('number', 'changeDate');
 
-    document.getElementById('loading_status').innerHTML = "loading team members' history...";
-    Promise.all(ratingPromises)
-      .then(histRating => {
-        // console.log(histRating);
-        document.getElementById('loading_status').innerHTML = "";
+        let ratingPromises = [];
+        for (let i = 0; i < members.length; i++) {
+          ratingPromises.push(getMemberRating(members[i], i));
+        }
 
-        const myGame = 'Puzzles';
-        let gameHistRating = [];
-        histRating.forEach((member) => {
-          member.history.forEach(game => {
-            if (game.name == myGame) {
-              gameHistRating.push({
-                member: member.member,
-                points: game.points
-              });
+        Promise.all(ratingPromises)
+          .then(histRating => {
+            // console.log(histRating);
+            document.getElementById('loading_status').innerHTML = "";
+
+            const myGame = 'Puzzles';
+            let gameHistRating = [];
+            histRating.forEach((member) => {
+              member.history.forEach(game => {
+                if (game.name == myGame) {
+                  gameHistRating.push({
+                    member: member.member,
+                    points: game.points
+                  });
+                }
+              })
+            });
+            console.log(gameHistRating);
+
+            for (let i = 0; i < gameHistRating.length; i++) {
+              data.addColumn('number', gameHistRating[i].member);
             }
+
+            let chartData = [];
+            for (let i = 0; i < 365; i++) {
+              let dateData = [i];
+              let targetDate = new Date();
+              let rankDate = new Date();
+              targetDate.setDate(targetDate.getDate() - i);
+              gameHistRating.forEach((member) => {
+                let points = 0;
+                member.points.forEach((rankChange) => {
+                  rankDate.setFullYear(rankChange[0]);
+                  rankDate.setMonth(rankChange[1]);
+                  rankDate.setDate(rankChange[2]);
+                  if (rankDate <= targetDate) {
+                    points = rankChange[3];
+                  }
+                })
+                dateData.push(points);
+                // 
+              });
+              chartData.push(dateData);
+            }
+
+            console.log(chartData);
+
+            data.addRows(chartData);
+
+            var options = {
+              hAxis: {
+                title: 'Days Ago',
+                direction: -1
+              },
+              vAxis: {
+                title: 'Rating'
+              },
+              backgroundColor: '#EFEFEF',
+              'title': 'SHP Chess Club Rating: ' + myGame,
+              'width': 1000,
+              'height': 750
+            };
+
+            var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+            chart.draw(data, options);
           })
-        });
-        console.log(gameHistRating);
-
-        for (let i = 0; i < gameHistRating.length; i++) {
-          data.addColumn('number', gameHistRating[i].member);
-        }
-
-        let chartData = [];
-        for (let i = 0; i < 365; i++) {
-          let dateData = [i];
-          let targetDate = new Date();
-          let rankDate = new Date();
-          targetDate.setDate(targetDate.getDate() - i);
-          gameHistRating.forEach((member) => {
-            let points = 0;
-            member.points.forEach((rankChange) => {
-              rankDate.setFullYear(rankChange[0]);
-              rankDate.setMonth(rankChange[1]);
-              rankDate.setDate(rankChange[2]);
-              if (rankDate <= targetDate) {
-                points = rankChange[3];
-              }
-            })
-            dateData.push(points);
-            // 
-          });
-          chartData.push(dateData);
-        }
-
-        console.log(chartData);
-
-        data.addRows(chartData);
-
-        var options = {
-          hAxis: {
-            title: 'Days Ago',
-            direction: -1
-          },
-          vAxis: {
-            title: 'Rating'
-          },
-          backgroundColor: '#EFEFEF',
-          'title': 'SHP Chess Club Rating: ' + myGame,
-          'width': 1000,
-          'height': 750
-        };
-
-        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-
-      })
-      .catch(
-        err => {
-          console.log(err);
-        }
-      );
-  })
+          .catch(
+            err => {
+              console.log(err);
+            }
+          );
+      });
+    })
     .catch(
       err => {
         console.log(err);
